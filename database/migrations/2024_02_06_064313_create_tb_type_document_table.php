fc0eaf2be159b765689b81a89635dd37b1024f1a
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-02-06 14:46:12 +0800

    create type document migration

---
+++ database/migrations/2024_02_06_064313_create_tb_type_document_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tb_type_document', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->foreignUuid('id_user')->nullable()->constrained('users');
+            $table->string('name_document');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tb_type_document');
+    }
+};
