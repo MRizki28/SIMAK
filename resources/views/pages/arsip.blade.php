9c68d576e99751b2317f8883c29f57449e9c8454
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-02-12 17:46:11 +0800

    fix style if data not found

--- resources/views/pages/arsip.blade.php
+++ resources/views/pages/arsip.blade.php
@@ -64,14 +64,18 @@
                                 <tbody class="text-center">
 
                                 </tbody>
-
+                                <tfoot id="dataNotFound">
+                                    <tr class="text-center text-muted" id="template-empty-info">
+                                        <td colspan="9" class=" ">
+                                            <i class="fas fa-folder-open mr-1"></i> File tidak ditemukan ...
+                                        </td>
+                                    </tr>
+                                </tfoot>
                             </table>
                             <div class="d-flex justify-content-between align-items-center px-4">
-
                                 <span class="mb-3 text-muted">
                                     Total <span id="data-total">...</span> data
                                 </span>
-
                                 <ul class="pagination pg-info"></ul>
                             </div>
                         </div>
@@ -164,11 +168,12 @@
         $(document).ready(function() {
 
             dateRangePickerSetup($('#datetime'))
+
             function paramsSearch() {
                 const search = $("#form-search").val();
                 const startDate = $("#datetime").data("start-date");
                 const endDate = $("#datetime").data("end-date");
-                
+
                 return {
                     search,
                     startDate,
@@ -248,10 +253,12 @@
                                 tableBody += "</tr>";
 
                                 $("#table tbody").append(tableBody);
+                                $("#dataNotFound").hide();
                             });
                             paginationLink(pagination, response);
                         } else {
                             $("#table tbody").empty();
+                            $("#dataNotFound").show();
                             pagination.empty();
                         }
                     }
