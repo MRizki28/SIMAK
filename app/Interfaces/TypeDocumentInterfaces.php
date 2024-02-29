09165a996ed2ac38df1f64a5ef009688ac749ea9
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-02-29 18:06:09 +0800

    fix create data by user

--- app/Interfaces/TypeDocumentInterfaces.php
+++ app/Interfaces/TypeDocumentInterfaces.php
@@ -8,6 +8,7 @@ interface TypeDocumentInterfaces {
     public function getAllData();
     public function createData(TypeDocumentRequest $request);
     public function getDataById($id);
+    public function getDataByUser();
     public function updateData(TypeDocumentRequest $request, $id);
     public function deleteData($id);
 }
