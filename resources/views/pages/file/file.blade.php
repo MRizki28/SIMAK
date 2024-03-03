56fc6a34cb3b55fdd05f18ecfe4ec4fdbebc5916
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-03-03 17:25:55 +0800

    fix create data arsip

--- resources/views/pages/file/file.blade.php
+++ resources/views/pages/file/file.blade.php
@@ -1,8 +1,21 @@
 @extends('Layouts.Base')
 @section('content')
     <div class="page-inner">
-        <div class="page-header ">
+        <div class="page-header">
             <h4 class="page-title">File</h4>
+            <ul class="breadcrumbs">
+                <li class="nav-home">
+                    <a href="/">
+                        <i class="flaticon-home"></i>
+                    </a>
+                </li>
+                <li class="separator">
+                    <i class="flaticon-right-arrow"></i>
+                </li>
+                <li class="nav-item">
+                    <span>File</span>
+                </li>
+            </ul>
         </div>
         <div class="row">
             <div class="col-md-12">
@@ -10,15 +23,8 @@
                     <div class="card-header">
                     </div>
                     <div class="card-body">
-                        <div class="row ">
-                            <div class="d-flex flex-column mx-3 mt-4">
-                                <i class="fas fa-file-archive fa-2xl text-center mb-1" ></i>
-                                <span>Nama file.jpg</span>
-                            </div>
-                            <div class="d-flex flex-column mx-3 mt-4">
-                                <i class="fas fa-file-archive fa-2xl text-center mb-1" ></i>
-                                <span>Nama file.jpg</span>
-                            </div>
+                        <div class="row " id="content">
+
                         </div>
                     </div>
                 </div>
@@ -33,16 +39,45 @@
                     $("#dataTable").DataTable().destroy();
                 }
                 let dataTable = $("#dataTable").DataTable();
+                let segments = window.location.pathname.split('/');
+                console.log(segments)
+                let idUser = segments[2];
+                let idArsip = segments[3];
                 $.ajax({
-                    type: "get",
-                    url: "url",
-                    data: "data",
-                    dataType: "dataType",
+                    type: "GET",
+                    url: "{{ url('api/v1/arsip/file/') }}/" + idUser + "/" + idArsip,
+                    dataType: "json",
                     success: function(response) {
-
+                        console.log(response)
+                        let files = response.data;
+                        let contentBody = "";
+                        $.each(files, function(index, file) {
+                            contentBody +=
+                                `<a href="/uploads/arsip/${file.file_arsip}" class="d-flex flex-column mx-3 mt-4" style="color:#575962;" target="_blank">`;
+                            if (file.file_arsip.toLowerCase().endsWith('.pdf')) {
+                                contentBody +=
+                                    '<i class="fas fa-file-pdf fa-2xl text-center mb-1" style="color:red;"></i>';
+                            } else if (file.file_arsip.toLowerCase().endsWith('.docx') || file
+                                .file_arsip.toLowerCase().endsWith('.doc')) {
+                                contentBody +=
+                                    '<i class="fas fa-file-word fa-2xl text-center mb-1"></i>';
+                            } else if (file.file_arsip.toLowerCase().endsWith('.xlsx') || file
+                                .file_arsip.toLowerCase().endsWith('.xls')) {
+                                contentBody +=
+                                    '<i class="fas fa-file-excel fa-2xl text-center mb-1"></i>';
+                            } else {
+                                contentBody +=
+                                    '<i class="fas fa-file-archive fa-2xl text-center mb-1" style="color:#F2AC34;"></i>';
+                            }
+                            contentBody += '<span>' + file.file_arsip + '</span>';
+                            contentBody += '</a>';
+                        });
+                        $('#content').append(contentBody)
                     }
                 });
             }
+
+            getFile();
         });
     </script>
 @endsection
