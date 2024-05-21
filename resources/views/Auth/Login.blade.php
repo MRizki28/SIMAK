ce35d9bb4947d71cd731d9987dd6e0926392aa91
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-05-21 10:25:02 +0800

    fix(favicon): change favicon

--- resources/views/Auth/Login.blade.php
+++ resources/views/Auth/Login.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="key" content="{{ env('APP_KEY') }}">
+    <link rel="icon" href="{{ asset('img/logo/favicon_palukota.png') }}" type="image/svg+xml" />
     <title>SIMAK - LOGIN</title>
     @include('Layouts.styles')
 </head>
