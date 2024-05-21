9f50a8470e7d4e7c5904ede6d24391ae06453c74
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-05-21 10:25:10 +0800

    fix(favicon): change favicon

--- resources/views/Layouts/Base.blade.php
+++ resources/views/Layouts/Base.blade.php
@@ -7,7 +7,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="key" content="{{ env('APP_KEY') }}">
     <meta content='width=device-width, initial-scale=1.0, shrink-to-fit=no' name='viewport' />
-    <link rel="icon" href="{{ asset('static/img/logo.svg') }}" type="image/svg+xml" />
+    <link rel="icon" href="{{ asset('img/logo/favicon_palukota.png') }}" type="image/svg+xml" />
     @include('Layouts.styles')
 </head>
 
