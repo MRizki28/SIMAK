d4e4db1a8477bd784b356c5524a23179c6ebfadb
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-06-24 11:02:44 +0800

    fix(query):fix new query and optimize

--- app/Interfaces/ArsipInterfaces.php
+++ app/Interfaces/ArsipInterfaces.php
@@ -17,4 +17,5 @@ interface ArsipInterfaces
     public function addFile(ArsipRequest $request);
     public function getDataArsipByPersonal(Request $request);
     public function getDataArsipByEntire(Request $request);
+    public function getYearArsip(Request $request);
 }
