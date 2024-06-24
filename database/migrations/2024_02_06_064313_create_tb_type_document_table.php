d53a9e00db733b6a23c24a4aed35b8327e1f8c8e
MRizki28 <muhammadrizkitkj123@gmail.com>
2024-06-24 11:03:25 +0800

    fix(query):fix new query and optimize

--- database/migrations/2024_02_06_064313_create_tb_type_document_table.php
+++ database/migrations/2024_02_06_064313_create_tb_type_document_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('tb_type_document', function (Blueprint $table) {
             $table->uuid('id')->primary();
-            $table->foreignUuid('id_user')->constrained('users');
             $table->string('name_type_document', 100);
             $table->timestamps();
         });
